8603b26f5d8a3be5398fdbc85b452c5c3ad5ae0f
maksm <maksmatsiaka@gmail.com>
2024-02-05 15:58:29 +0200

    the end of the task with a description

--- src/components/generate_description/generate_description.js
+++ src/components/generate_description/generate_description.js
@@ -1,20 +1,23 @@
 import s from "./generate_description.module.scss"
 import {useState} from "react";
 
-export const GenerateDescription = () => {
+export const GenerateDescription = ({keyWords, img_url}) => {
     const [description, setDescription] = useState('')
 
     const getDescription = () => {
         fetch("/api/generate_post_description", {
-            body: ({
-
+            body: JSON.stringify({
+                img_url: img_url,
+                keyWords: keyWords
             }),
             method: "POST",
             headers: {
                 "Content-Type": "application/json",
             },
         }).then(res => {
-            console.log(res)
+            return res.json();
+        }).then(data => {
+            setDescription(data.choices[0].message.content)
         })
     }
 
