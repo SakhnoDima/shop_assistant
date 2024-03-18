02a7461db821f7f62a36500bef4cc647837c5bd2
SakhnoDima <134371251+SakhnoDima@users.noreply.github.com>
2024-03-18 10:06:57 +0200

    remove ai

--- src/app/api/assist/route.js
+++ src/app/api/assist/route.js
@@ -105,53 +105,53 @@ const assistantFilesUploader = async () => {
 };
 
 export const POST = async (request, response) => {
-  const { userMessage } = await request.json();
+  // const { userMessage } = await request.json();
 
-  // await assistantFilesUploader();
+  // // await assistantFilesUploader();
 
-  // ==========  Step 1: Create a Thread
-  const thread = await openai.beta.threads.create();
-  //Step 2: Add a Message to a Thread
-  await openai.beta.threads.messages.create(thread.id, {
-    role: "user",
-    content: userMessage,
-  });
-  //Step 3: Run the Assistant
-  const run = await openai.beta.threads.runs.create(thread.id, {
-    assistant_id: process.env.ASSISTANT_ID,
-  });
+  // // ==========  Step 1: Create a Thread
+  // const thread = await openai.beta.threads.create();
+  // //Step 2: Add a Message to a Thread
+  // await openai.beta.threads.messages.create(thread.id, {
+  //   role: "user",
+  //   content: userMessage,
+  // });
+  // //Step 3: Run the Assistant
+  // const run = await openai.beta.threads.runs.create(thread.id, {
+  //   assistant_id: process.env.ASSISTANT_ID,
+  // });
 
-  const checkStatus = async (threadId, runId) => {
-    let runStatus = await openai.beta.threads.runs.retrieve(threadId, runId);
+  // const checkStatus = async (threadId, runId) => {
+  //   let runStatus = await openai.beta.threads.runs.retrieve(threadId, runId);
 
-    while (runStatus.status !== "completed") {
-      await new Promise((resolve) => setTimeout(resolve, 1000));
-      runStatus = await openai.beta.threads.runs.retrieve(threadId, runId);
+  //   while (runStatus.status !== "completed") {
+  //     await new Promise((resolve) => setTimeout(resolve, 1000));
+  //     runStatus = await openai.beta.threads.runs.retrieve(threadId, runId);
 
-      if (runStatus.status === "requires_action") {
-        const actions = runStatus.required_action.submit_tool_outputs;
-        for (const action of actions["tool_calls"]) {
-          const fooName = action.function.name;
-          const arg = action.function.arguments;
+  //     if (runStatus.status === "requires_action") {
+  //       const actions = runStatus.required_action.submit_tool_outputs;
+  //       for (const action of actions["tool_calls"]) {
+  //         const fooName = action.function.name;
+  //         const arg = action.function.arguments;
 
-          if (arg.length === 2) {
-            return "Add your phone and email pleas";
-          }
+  //         if (arg.length === 2) {
+  //           return "Add your phone and email pleas";
+  //         }
 
-          if (fooName === "save_user_data") {
-            const res = await saveUserData(arg);
-            return res;
-          }
-        }
-      }
-    }
+  //         if (fooName === "save_user_data") {
+  //           const res = await saveUserData(arg);
+  //           return res;
+  //         }
+  //       }
+  //     }
+  //   }
 
-    let messages = await openai.beta.threads.messages.list(threadId);
-    const answer = messages.data.filter((el) => el.role === "assistant");
-    return answer[0].content[0].text.value;
-  };
+  //   let messages = await openai.beta.threads.messages.list(threadId);
+  //   const answer = messages.data.filter((el) => el.role === "assistant");
+  //   return answer[0].content[0].text.value;
+  // };
 
-  let data = await checkStatus(thread.id, run.id);
+  // let data = await checkStatus(thread.id, run.id);
 
-  return NextResponse.json({ resMess: data });
+  return NextResponse.json({ resMess: "Ok" });
 };
