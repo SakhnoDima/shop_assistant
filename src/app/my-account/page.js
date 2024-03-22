2501bafae2170f6a6f9436e24d57232849f31849
SakhnoDima <134371251+SakhnoDima@users.noreply.github.com>
2024-03-22 16:09:12 +0200

    11

--- src/app/my-account/page.js
+++ src/app/my-account/page.js
@@ -1,10 +1,14 @@
-import {Loader} from "@/components/loader/Loader";
+import { Loader } from "@/components/loader/Loader";
 
 const MyAccount = () => {
-
-    return <>My account
-        {}
-    </>
-}
+  console.log(11);
+  console.log(process.env.CURRENT_URL);
+  return (
+    <div>
+      My account
+      {process.env.CURRENT_URL}
+    </div>
+  );
+};
 
 export default MyAccount;
